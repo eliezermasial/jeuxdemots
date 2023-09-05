819bc00994b52a8732bb0068c51721cff75748b9
unknown <eliezermasiala2000@gmail.com>
2023-09-05 16:32:49 +0200

    first commit

---
+++ contact.js
@@ -0,0 +1,70 @@
+
+// les variables utilisés pour les jeux
+const listesMots = ["poulet","elie","tamba","masiala"]
+const listesPhrases = ["vient manger","pourquoi tu es triste","vas a l'ecole","a quel heure viendras-tu"]
+const zoneSaisie = document.querySelector(".zoneSaisie input")
+
+let zoneProposition = document.querySelector(".zoneProposition")
+let zoneScore = document.querySelector(".zoneScore span")
+let button = document.getElementById("btnValiderMot")
+let optionSource = document.querySelectorAll(`input[name="optionSource"]`)
+
+
+// les  variables a incrumenter
+let score = 0;
+let i = 0;
+let couleur= ""
+
+
+function lencerJeux(){
+    for(let y = 0; y < optionSource.length; y++){
+
+        optionSource[y].addEventListener("click",()=>{
+    
+            if(optionSource[y].checked){
+                let check = optionSource[y].value
+                
+                if(check === "1"){
+                    affiche()
+                    console.log("bienvenu")
+                } else{
+                    console.log(check)
+                }
+            }
+        })
+        
+    }
+}
+
+
+function affiche(){
+
+    zoneProposition.innerHTML = listesMots[i++]
+    
+    // action effectuer par le button
+        
+        button.addEventListener("click",()=>{
+    
+            zoneProposition.innerHTML = listesMots[i++]
+        
+            if(zoneProposition.innerHTML === "undefined"){
+                zoneProposition.innerHTML = "le jeux est fini"
+                zoneScore.innerHTML = `${score}/${listesMots.length}`
+                button.disabled = true;
+                
+            }
+    
+            score++
+            
+        })
+}
+    
+
+
+
+lencerJeux()
+
+
+
+
+
