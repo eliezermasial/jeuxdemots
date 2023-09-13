3ec3b45be6d375bd29c682e5649f16f797fc5dd5
unknown <eliezermasiala2000@gmail.com>
2023-09-13 15:31:34 +0200

    creation de fichier main, variables,

---
+++ scripts/variables.js
@@ -0,0 +1,6 @@
+// les constants necessaires utilisés pour le jeu
+const body = document.querySelector("body")
+const listesMots = ["poulet","elie","tamba","masiala"]
+const listesPhrases = ["vient manger","pourquoi tu es triste","vas a l'ecole","a quel heure viendras-tu"]
+const zoneSaisie = document.querySelector(".zoneSaisie input")
+const btnPartager = document.getElementById("btnPartager")
